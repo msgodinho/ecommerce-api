0b24ac5a707c52f0b66f3bb0fcb29e1f09d91905
Moises Godinho <moisesathaydes@gmail.com>
2025-12-30 00:09:56 -0300

    adjusted conflicts

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,11 @@
 import { Joi } from "celebrate";
 
+export type User = {
+  id: string;
+  nome: string;
+  email: string;
+};
+
 export const userSchema = Joi.object().keys({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
